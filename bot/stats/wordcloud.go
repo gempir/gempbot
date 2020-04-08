db6c81141433ff763cf5e9e59eed1e6b2227aefe
gempir <daniel.pasch.s@gmail.com>
2020-04-08 21:53:22 +0200

    ignored words

--- bot/stats/wordcloud.go
+++ bot/stats/wordcloud.go
@@ -4,10 +4,32 @@ import (
 	"strings"
 )
 
+var (
+	ignoredWords = []string{"a", "the", "you", "i", "is", "to", "no", "yes"}
+)
+
 func (b *Broadcaster) worldcloudMessageHandler() {
 	go func() {
 		for message := range b.messageQueue {
-			b.store.AddToWordcloud(strings.Split(message.Message, " ")...)
+
+			words := []string{}
+
+			for _, word := range strings.Split(message.Message, " ") {
+				if !isIgnored(word) {
+					words = append(words, word)
+				}
+			}
+
+			b.store.AddToWordcloud(words...)
 		}
 	}()
+}
+
+func isIgnored(word string) bool {
+	for _, ignored := range ignoredWords {
+		if strings.ToLower(word) == strings.ToLower(ignored) {
+			return true
+		}
+	}
+	return false
 }
