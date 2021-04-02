dff24c2e11a06371f630a3dd095b5d6d297e7e52
gempir <gempir.dev@gmail.com>
2021-04-02 23:42:36 +0200

    split meta

--- web/src/components/Meta.tsx
+++ web/src/components/Meta.tsx
@@ -6,11 +6,17 @@ const MetaContainer = styled.div`
     border: 1px solid var(--bg-brighter);
     padding: 0.5rem;
     margin: 1rem;
+    margin-right: 0;
     margin-bottom: 0;
 `;
 
-export function Meta({activeChannels, joinedChannels}: {[key: string]: number}) {
-    return <MetaContainer>
-        Joined Channels: <strong>{joinedChannels}</strong> | Active Channels: <strong>{activeChannels}</strong>
-    </MetaContainer> 
+export function Meta({ activeChannels, joinedChannels }: { [key: string]: number }) {
+    return <>
+        <MetaContainer>
+            Joined Channels: <strong>{joinedChannels}</strong>
+        </MetaContainer>
+        <MetaContainer>
+            Active Channels: <strong>{activeChannels}</strong>
+        </MetaContainer>
+    </>
 }
