f3c99fe93a64e792929ea84a457a3fde22bbbaf1
gempir <gempir.dev@gmail.com>
2021-05-15 13:06:27 +0200

    cleanup managing code

--- web/src/components/Managing.tsx
+++ web/src/components/Managing.tsx
@@ -1,6 +1,5 @@
 import { UserConfig } from "../hooks/useUserConfig";
 import { store } from "../store";
-import "./Managing.css";
 
 export function Managing({ userConfig }: { userConfig: UserConfig | null | undefined }) {
     if (userConfig?.Protected.EditorFor.length === 0) {
@@ -9,7 +8,7 @@ export function Managing({ userConfig }: { userConfig: UserConfig | null | undef
     const managing = store.useState(s => s.managing);
 
     return <div className="Managing">
-        <select className="bg-gray-800 shadow rounded" onChange={e => store.update(s => { s.managing = e.target.value })} value={managing} defaultValue={""}>
+        <select className="bg-gray-800 shadow rounded p-3" onChange={e => store.update(s => { s.managing = e.target.value })} value={managing} defaultValue={""}>
             {userConfig?.Protected.EditorFor.map(editorFor => <option key={editorFor} value={editorFor}>{editorFor}</option>)}
             <option value="">you</option>
         </select>
