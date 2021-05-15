df606d77f3e940a7ac5bdb2a8fc1c0721c6046d5
gempir <gempir.dev@gmail.com>
2021-05-15 14:51:19 +0200

    create basic bttv form

--- web/src/components/RewardForms/BttvForm.tsx
+++ web/src/components/RewardForms/BttvForm.tsx
@@ -1,24 +1,34 @@
 import { useForm } from "react-hook-form";
 
 export function BttvForm() {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm();
     // @ts-ignore
     const onSubmit = data => console.log(data);
 
-    console.log(watch("example")); // watch input value by passing the name of it
-
     return (
-        /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
         <form onSubmit={handleSubmit(onSubmit)} className="m-4 p-4 bg-gray-800 rounded shadow">
-            {/* register your input into the hook by invoking the "register" function */}
-            <input defaultValue="Bttv Emote" {...register("name")} className="bg-gray-700 p-2 rounded shadow block" />
+            <label className="block">
+                Title
+                <input defaultValue="Bttv Emote" spellCheck={false} {...register("title", { required: true })} className="form-input border-none bg-gray-700 mx-2 p-2 rounded shadow" />
+                {errors.title && <span className="text-red-700">required</span>}
+            </label>
 
-            {/* include validation with required or other standard HTML validation rules */}
-            <input {...register("exampleRequired", { required: true })} className="bg-gray-700 p-2 rounded shadow block mt-3" />
-            {/* errors will return when field validation fails  */}
-            {errors.exampleRequired && <span>This field is required</span>}
+            <label className="block mt-3">
+                Cost 
+                <input defaultValue="Bttv Emote" type="number" spellCheck={false} {...register("cost", { required: true })} className="form-input border-none bg-gray-700 mx-2 p-2 rounded shadow" />
+                {errors.cost && <span className="text-red-700">required</span>}
+            </label>
 
-            <div className="flex justify-end">
+            <label className="block mt-3">
+                Prompt
+                <input spellCheck={false} disabled {...register("prompt")} className="block cursor-not-allowed truncate form-input w-full opacity-25 border-none bg-gray-700 mt-2 p-2 rounded shadow" defaultValue="Add a BetterTTV emote! In the text field, send a link to the BetterTTV emote. powered by spamchamp.gempir.com" />
+            </label>
+
+            <div className="flex flex-row justify-between items-center select-none">
+                <label className="inline-flex items-center">
+                    <input type="checkbox" className="form-checkbox rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-offset-0 focus:ring-indigo-200 focus:ring-opacity-50" />
+                    <span className="ml-2">Enabled</span>
+                </label>
                 <input type="submit" className="bg-green-700 hover:bg-green-600 p-2 rounded shadow block mt-3" value="save" />
             </div>
         </form>
