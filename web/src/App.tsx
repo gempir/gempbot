6b3aeadc4834621d8b16acf48f7635e1c72dec0a
gempir <gempir.dev@gmail.com>
2021-06-02 22:58:19 +0200

    add a privacy policy

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,11 @@
 import {
     BrowserRouter as Router,
-    Route, Switch
+    Route, Switch, Link
 } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import { Dashboard } from "./components/Dashboard/Dashboard";
 import { Home } from "./components/Home/Home";
+import { Privacy } from "./components/Privacy/Privacy";
 
 
 export function App() {
@@ -14,9 +15,15 @@ export function App() {
             <Route path="/dashboard">
                 <Dashboard />
             </Route>
+            <Route path="/privacy">
+                <Privacy />
+            </Route>
             <Route path="/">
                 <Home />
             </Route>
         </Switch>
+        <Link to="/privacy" className="fixed bottom-3 right-3 hover:text-gray-400">
+            Privacy
+        </Link>
     </Router>
 }
