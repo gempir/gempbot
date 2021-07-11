44212a91912f0ad2955161e0f95e380f0ebc6874
gempir <gempir.dev@gmail.com>
2021-07-11 15:57:11 +0200

    more bot handling

--- web/src/components/Sidebar/BotManager.tsx
+++ web/src/components/Sidebar/BotManager.tsx
@@ -4,14 +4,18 @@ import { Chat } from "../../icons/Chat";
 
 export function BotManager({ userConfig, setUserConfig }: { userConfig: UserConfig, setUserConfig: SetUserConfig }) {
     const classes = "p-3 flex justify-center rounded shadow cursor-pointer mt-2 w-full hover:opacity-100".split(" ")
+    const [hovering, setHovering] = useState(false);
 
     if (userConfig?.BotJoin) {
         classes.push(..."bg-green-900 hover:bg-red-800 focus:bg-green:800 opacity-25".split(" "));
     } else {
-        classes.push(..."bg-red-900 hover:bg-red-800 focus:bg-red:800".split(" "));
+        classes.push(..."bg-red-900 focus:bg-red:800".split(" "));
+
+        if (hovering) {
+            classes.push(..."hover:bg-green-900".split(" "));
+        }
     }
 
-    const [hovering, setHovering] = useState(false);
 
     return <div className={classes.join(" ")}
         onMouseEnter={() => setHovering(true)}
