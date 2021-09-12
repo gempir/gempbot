98bc3f8e2cc02d7d2f15404b7045874a5216b28a
gempir <gempir.dev@gmail.com>
2021-09-12 09:27:40 +0200

    fix functions hopefully

--- functions/hello/main.go
+++ functions/hello/main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func helloHandler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	return &events.APIGatewayProxyResponse{
 		StatusCode:      200,
 		Headers:         map[string]string{"Content-Type": "text/plain"},
-		Body:            "Hello, World!",
+		Body:            "Hello",
 		IsBase64Encoded: false,
 	}, nil
+}
+
+func main() {
+	lambda.Start(helloHandler)
 }
