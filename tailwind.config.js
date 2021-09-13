12823c3169079719df3bb4594f4c63851246f66f
gempir <gempir.dev@gmail.com>
2021-09-13 23:23:52 +0200

    merge conflicts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
 module.exports = {
-    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-    darkMode: 'media', // or 'media' or 'class'
-    theme: {
-        extend: {},
-    },
-    variants: {
-        extend: {},
-    },
-    plugins: [
-        require("@tailwindcss/forms")({
-            strategy: 'class',
-        }),
-    ],
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 }
