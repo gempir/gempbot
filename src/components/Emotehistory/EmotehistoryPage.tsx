fdbe9e5fd958e0538322429e15950205c1243bec
gempir <gempir.dev@gmail.com>
2021-09-14 23:04:06 +0200

    remove router

--- src/components/Emotehistory/EmotehistoryPage.tsx
+++ src/components/Emotehistory/EmotehistoryPage.tsx
@@ -1,10 +1,9 @@
-import { useParams } from "react-router-dom";
 import { Emotehistory } from "../Home/Emotehistory";
 
 export function EmotehistoryPage() {
-    const { channel } = useParams<{channel: string}>();
+    // const { channel } = useParams<{channel: string}>();
 
     return <div className="p-4 w-full">
-        <Emotehistory channel={channel} />
+        <Emotehistory channel={"channel"} />
     </div>;
 }
--- src/components/Predictionhistory/PredictionhistoryPage.tsx
+++ src/components/Predictionhistory/PredictionhistoryPage.tsx
@@ -1,10 +1,9 @@
-import { useParams } from "react-router-dom";
 import { PredictionLog } from "../Home/PredictionLog";
 
 export function PredictionhistoryPage() {
-    const { channel } = useParams<{channel: string}>();
+    // const { channel } = useParams<{channel: string}>();
 
     return <div className="p-4 w-full"> 
-        <PredictionLog channel={channel} />
+        <PredictionLog channel={"channel"} />
     </div>;
 }
