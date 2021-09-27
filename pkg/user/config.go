385981d8f1ba60692bd17c9e8f86d4a2383d4852
gempir <gempir.dev@gmail.com>
2021-09-28 00:29:24 +0200

    actually save

--- pkg/user/config.go
+++ pkg/user/config.go
@@ -201,16 +201,11 @@ func (u *UserAdmin) ProcessConfig(ctx context.Context, userID string, login stri
 		}
 	}
 
-	// dbErr := u.db.SaveBotConfig(ctx, store.BotConfig{OwnerTwitchID: ownerUserID, JoinBot: newUserIDConfig.BotJoin})
-	// if dbErr != nil {
-	// 	log.Error(dbErr)
-	// 	return api.NewApiError(http.StatusInternalServerError, fmt.Errorf("failed to save bot config"))
-	// }
-	// if newConfig.BotJoin {
-	// 	u.store.PublishIngesterMessage(store.IngesterMsgJoin, ownerLogin)
-	// } else {
-	// 	u.store.PublishIngesterMessage(store.IngesterMsgPart, ownerLogin)
-	// }
+	dbErr := u.db.SaveBotConfig(ctx, store.BotConfig{OwnerTwitchID: ownerUserID, JoinBot: newUserIDConfig.BotJoin})
+	if dbErr != nil {
+		log.Error(dbErr)
+		return api.NewApiError(http.StatusInternalServerError, fmt.Errorf("failed to save bot config"))
+	}
 	if newConfig.BotJoin {
 		u.chatClient.Say(u.cfg.Username, fmt.Sprintf("JOIN %s", ownerLogin))
 	} else {
