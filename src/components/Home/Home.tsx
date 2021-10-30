cd59a26be58e88a1b586727ec2dfa9c2eb9b0767
gempir <gempir.dev@gmail.com>
2021-10-30 19:26:18 +0200

    remove unused

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,7 @@
-import { useTitle } from "react-use";
 import { Emotehistory } from "./Emotehistory";
-import { PredictionLog } from "./PredictionLog";
 
 export function Home() {
     return <div className="p-4 w-full max-h-screen flex gap-4">
         <Emotehistory />
-        <PredictionLog />
     </div>
 }
