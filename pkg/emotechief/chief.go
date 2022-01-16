fa7da791ffebd96dc9b87b9147d16d6aa2be8276
gempir <gempir.dev@gmail.com>
2022-01-16 17:25:49 +0100

    unused http client

--- pkg/emotechief/chief.go
+++ pkg/emotechief/chief.go
@@ -1,8 +1,6 @@
 package emotechief
 
 import (
-	"net/http"
-
 	"github.com/gempir/gempbot/pkg/chat"
 	"github.com/gempir/gempbot/pkg/config"
 	"github.com/gempir/gempbot/pkg/helixclient"
@@ -13,7 +11,6 @@ type EmoteChief struct {
 	cfg         *config.Config
 	db          *store.Database
 	helixClient *helixclient.Client
-	httpClient  *http.Client
 	chatClient  *chat.ChatClient
 }
 
@@ -22,7 +19,6 @@ func NewEmoteChief(cfg *config.Config, db *store.Database, helixClient *helixcli
 		cfg:         cfg,
 		db:          db,
 		helixClient: helixClient,
-		httpClient:  &http.Client{},
 		chatClient:  chatClient,
 	}
 }
