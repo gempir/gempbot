d9ebd62608380bcb4a6da97b691919190b5f1270
gempir <gempir.dev@gmail.com>
2022-10-01 14:57:08 +0200

    setup some value functions for seventv emote sets

--- internal/emoteservice/seventv.go
+++ internal/emoteservice/seventv.go
@@ -11,6 +11,7 @@ import (
 )
 
 const DefaultSevenTvApiBaseUrl = "https://api.7tv.app/v2"
+const DefaultSevenTvV3ApiBaseUrl = "https://7tv.io/v3"
 const DefaultSevenTvGqlBaseUrl = "https://api.7tv.app/v2/gql"
 
 type SevenTvClient struct {
@@ -25,6 +26,63 @@ func NewSevenTvClient(store store.Store) *SevenTvClient {
 		apiBaseUrl: DefaultSevenTvApiBaseUrl,
 		gqlBaseUrl: DefaultSevenTvGqlBaseUrl,
 	}
+}
+
+type UserV3 struct {
+	ID                string `json:"id"`
+	Username          string `json:"username"`
+	ProfilePictureURL string `json:"profile_picture_url"`
+	DisplayName       string `json:"display_name"`
+	Style             struct {
+		Color int         `json:"color"`
+		Paint interface{} `json:"paint"`
+	} `json:"style"`
+	Biography string `json:"biography"`
+	Editors   []struct {
+		ID          string `json:"id"`
+		Permissions int    `json:"permissions"`
+		Visible     bool   `json:"visible"`
+		AddedAt     int64  `json:"added_at"`
+	} `json:"editors"`
+	Roles       []string `json:"roles"`
+	Connections []struct {
+		ID            string `json:"id"`
+		Platform      string `json:"platform"`
+		Username      string `json:"username"`
+		DisplayName   string `json:"display_name"`
+		LinkedAt      int64  `json:"linked_at"`
+		EmoteCapacity int    `json:"emote_capacity"`
+	} `json:"connections"`
+}
+
+func (c *SevenTvClient) GetUserV3(userID string) (UserV3, error) {
+	// first figure out the bttvUserId for the channel, might cache this later on
+	var userResp UserV3
+	err := requests.
+		URL(DefaultSevenTvV3ApiBaseUrl).
+		Pathf("/users/%s", userID).
+		ToJSON(&userResp).
+		Fetch(context.Background())
+	if err != nil {
+		return UserV3{}, err
+	}
+
+	return userResp, nil
+}
+
+func (c *SevenTvClient) GetTwitchConnection(userID string) (string, error) {
+	user, err := c.GetUserV3(userID)
+	if err != nil {
+		return "", err
+	}
+
+	for _, connection := range user.Connections {
+		if connection.Platform == "TWITCH" {
+			return connection.Username, nil
+		}
+	}
+
+	return "", fmt.Errorf("no twitch connection found for user %s", userID)
 }
 
 func (c *SevenTvClient) GetEmote(emoteID string) (Emote, error) {
