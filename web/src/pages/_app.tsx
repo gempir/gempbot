5fffb74ba73f3419157c83572f868ffa8578b087
gempir <gempir.dev@gmail.com>
2022-10-21 20:10:12 +0200

    change title

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }: { Component: any; pageProp
 
     return <>
         <Head>
-            <title>gempbot</title>
+            <title>gempbot - fly.io</title>
             <link rel="icon" href="/favicon.ico" />
         </Head>
         <style jsx global>{`
