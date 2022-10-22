75046f08674cf25d011670c84896397550c1cd60
gempir <gempir.dev@gmail.com>
2022-10-22 17:58:28 +0200

    add time cols

--- internal/store/media.go
+++ internal/store/media.go
@@ -1,6 +1,10 @@
 package store
 
+import "time"
+
 type MediaPlayer struct {
 	ChannelTwitchId string `gorm:"primaryKey"`
 	CurrentTime     float32
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
 }
