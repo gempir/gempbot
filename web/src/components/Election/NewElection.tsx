14fb05ce345e79c4f26a7b3fef064f22bfb280e5
gempir <gempir.dev@gmail.com>
2022-12-04 10:21:08 +0100

    form for election

--- web/src/components/Election/NewElection.tsx
+++ web/src/components/Election/NewElection.tsx
@@ -1,18 +1,26 @@
 import { SubmitHandler, useForm } from "react-hook-form";
-import React from "react";
 
 type FormValues = {
-    title: string;
+    hours: number;
+    nominationCost: number;
 };
 
 export function NewElection() {
     const { register, handleSubmit, watch, formState: { errors } } = useForm<FormValues>();
     const onSubmit: SubmitHandler<FormValues> = data => console.log(data);
 
-    return <form onSubmit={handleSubmit(onSubmit)}>
-        <input defaultValue="test" {...register("title", { required: true })} className="form-input border-none bg-gray-700 mx-2 p-2 rounded shadow" />
-        {errors.title && <span>This field is required</span>}
+    return <form onSubmit={handleSubmit(onSubmit)} className="p-4 bg-gray-800 rounded shadow relative flex flex-col">
+        <h2 className="mb-5 text-xl font-bold">Create a new 7tv emote election</h2>
+        <label>
+            Every X Hours
+            <input defaultValue="24" {...register("hours", { required: true })} className="form-input border-none bg-gray-700 mx-2 p-2 rounded shadow" />
+        </label>
+        <br />
+        <label>
+            Nomination Cost
+            <input defaultValue="5000" {...register("nominationCost", { required: true })} className="form-input border-none bg-gray-700 mx-2 p-2 rounded shadow" />
+        </label>
 
-        <input type="submit" />
+        <input type="submit" className="hover:bg-gray-600 bg-gray-700 p-3 shadow rounded cursor-pointer font-bold mt-4 w-20 flex justify-center" />
     </form>;
 }
