bb60d66e5f8623716633239d9cb027ec3aaf5449
gempir <gempir.dev@gmail.com>
2022-12-23 17:18:45 +0100

    xd

--- internal/store/db.go
+++ internal/store/db.go
@@ -60,7 +60,23 @@ func NewDatabase(cfg *config.Config) *Database {
 
 func (db *Database) Migrate() {
 	log.Info("Migrating schema")
-	err := db.Client.AutoMigrate(SystemConfig{}, ChannelPointReward{}, EventSubSubscription{}, UserAccessToken{}, AppAccessToken{}, EmoteAdd{}, BotConfig{}, Permission{}, EventSubMessage{}, EmoteBlock{}, MediaPlayer{}, MediaQueue{}, Election{}, Nomination{}, NominationVote{})
+	err := db.Client.AutoMigrate(
+		SystemConfig{},
+		ChannelPointReward{},
+		EventSubSubscription{},
+		UserAccessToken{},
+		AppAccessToken{},
+		EmoteAdd{},
+		BotConfig{},
+		Permission{},
+		EventSubMessage{},
+		EmoteBlock{},
+		MediaPlayer{},
+		MediaQueue{},
+		Election{},
+		Nomination{},
+		NominationVote{},
+	)
 	if err != nil {
 		panic("Failed to migrate, " + err.Error())
 	}
