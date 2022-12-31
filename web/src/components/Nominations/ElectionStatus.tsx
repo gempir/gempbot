865c693198c7491d1f653ffdb5e18c08515303be
gempir <gempir.dev@gmail.com>
2022-12-31 15:50:03 +0100

    cleanup old nymn subs

--- web/src/components/Nominations/ElectionStatus.tsx
+++ web/src/components/Nominations/ElectionStatus.tsx
@@ -1,14 +1,24 @@
 import dayjs from "dayjs";
 import { useEffect, useState } from "react";
+import { useForceUpdate } from "../../hooks/useForceUpdate";
 import { isSSR } from "../../service/isSSR";
 import { Election } from "../../types/Election";
 
 export function ElectionStatus({ election }: { election?: Election }): JSX.Element | null {
     const [renderAllowed, setRenderAllowed] = useState(false);
+    const forceUpdate = useForceUpdate();
 
     useEffect(() => {
         if (!isSSR()) {
             setRenderAllowed(true);
+        }
+
+        const interval = setInterval(() => {
+            forceUpdate();
+        }, 60000);
+
+        return () => {
+            clearInterval(interval);
         }
     }, []);
 
@@ -17,40 +27,61 @@ export function ElectionStatus({ election }: { election?: Election }): JSX.Eleme
         const endingTime = election.StartedRunAt.add(election.Hours, 'hour');
         if (election.SpecificTime) {
             if (election.SpecificTime.isAfter(endingTime)) {
-                endingAt = election.SpecificTime.format("L LT");
+                endingAt = election.SpecificTime;
             } else {
-                endingAt = election.SpecificTime.add(1, 'day').format("L LT");
+                endingAt = election.SpecificTime.add(1, 'day');
             }
         } else {
-            endingAt = endingTime.format("L LT");
+            endingAt = endingTime;
         }
     }
     let startedAt;
     if (renderAllowed) {
-        startedAt = election?.StartedRunAt?.format("L LT");
+        startedAt = election?.StartedRunAt;
 
         if (!election?.StartedRunAt && election?.SpecificTime) {
             const specificTime = dayjs().set("hour", election.SpecificTime.hour()).set("minute", election.SpecificTime.minute());
             if (specificTime.isBefore(dayjs())) {
-                startedAt = specificTime.add(1, 'day').format("L LT");
+                startedAt = specificTime.add(1, 'day');
             } else {
-                startedAt = specificTime.format("L LT");
+                startedAt = specificTime;
             }
+        }
+    }
+
+    let countdown = "";
+
+    if (renderAllowed && endingAt) {
+        let minutes = endingAt.diff(dayjs(), 'minutes');
+
+        const days = Math.floor(minutes / 1440);
+        minutes -= days * 1440;
+        const hours = Math.floor(minutes / 60);
+        minutes -= hours * 60;
+
+        if (days) {
+            countdown += `${days} day${days > 1 ? "s" : ""} `;
+        }
+        if (hours) {
+            countdown += `${hours} hour${hours > 1 ? "s" : ""} `;
+        }
+        if (minutes) {
+            countdown += `${minutes} minute${minutes > 1 ? "s" : ""} `;
         }
     }
 
     return <div>
         <div>
-            <span className="text-gray-400">Start at ~</span> <strong>{startedAt}</strong>
+            <span className="text-gray-400">Start at ~</span> {startedAt?.format("L LT")}
         </div>
         <div>
-            <span className="text-gray-400">End at ~</span> <strong>{endingAt}</strong>
+            <span className="text-gray-400">End at ~</span> {endingAt?.format("L LT")}
         </div>
         <div>
-            <span className="text-gray-400">Duration</span> {!!election && <strong>{election.Hours} hours</strong>}
+            <span className="text-gray-400">Top</span> {!!election && election.EmoteAmount}
         </div>
         <div>
-            <span className="text-gray-400">Top</span> {!!election && <strong>{election.EmoteAmount}</strong>}
+            <span className="text-gray-400">Ending in ~</span> <strong>{countdown}</strong>
         </div>
     </div>;
 }
