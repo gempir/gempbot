539bbc73ab97c4acc787c708bf41eb0aa4a92bf3
gempir <gempir.dev@gmail.com>
2024-02-10 14:44:27 +0100

    increase size

--- web/yjs/server.js
+++ web/yjs/server.js
@@ -5,8 +5,8 @@ import josnwebtoken from 'jsonwebtoken';
 import WebSocket from 'ws';
 import { setupWSConnection } from './util.cjs';
 
-// max payload size is 128MB
-const wss = new WebSocket.Server({ noServer: true, maxPayload: 1024 * 1024 * 128});
+// max payload size is 512MB
+const wss = new WebSocket.Server({ noServer: true, maxPayload: 1024 * 1024 * 512});
 
 const host = process.env.HOST ?? '127.0.0.1'
 const port = process.env.PORT ?? 1234
