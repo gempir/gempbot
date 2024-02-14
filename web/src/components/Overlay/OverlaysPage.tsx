bc9162cc1e8b594236ef01a70c4a212858048ff7
gempir <gempir.dev@gmail.com>
2024-02-14 19:56:18 +0100

    clean up

--- web/src/components/Overlay/OverlaysPage.tsx
+++ web/src/components/Overlay/OverlaysPage.tsx
@@ -7,26 +7,26 @@ const gempirId = "77829817";
 export function OverlaysPage() {
     const [overlays, addOverlay, deleteOverlay, errorMessage, loading] = useOverlays();
     const [user] = useUserConfig();
-
+    const isGempir = user?.Protected.CurrentUserID === gempirId;
 
     return <div className="relative w-full h-[100vh] p-4">
         <div className="p-4 bg-gray-800 rounded shadow max-w-[800px]">
             <div className="group">
-                <button disabled={user?.Protected.CurrentUserID !== gempirId} onClick={addOverlay} className="bg-green-700 hover:bg-green-600 p-2 rounded shadow block cursor-pointer">Add Overlay</button>
-                <span className="absolute z-50 hidden mt-2 p-2 w-48 text-center bg-black/75 text-white rounded tooltip-text group-hover:block pointer-events-none">
+                <button disabled={!isGempir} onClick={addOverlay} className="bg-green-700 hover:bg-green-600 p-2 rounded shadow block cursor-pointer">Add Overlay</button>
+                {!isGempir && <span className="absolute z-50 hidden mt-2 p-2 w-48 text-center bg-black/75 text-white rounded tooltip-text group-hover:block pointer-events-none">
                     Only gempir can
-                </span>
+                </span>}
             </div>
             <div className="mt-5">
                 {overlays.map(overlay => <div key={overlay.ID} className="flex items-center justify-between p-4 bg-gray-900">
                     <div>
                         <div className="group">
-                            <button className="bg-red-700 hover:bg-red-600 p-2 rounded shadow block cursor-pointer" disabled={user?.Protected.CurrentUserID !== gempirId} onClick={() => {
+                            <button className="bg-red-700 hover:bg-red-600 p-2 rounded shadow block cursor-pointer" disabled={!isGempir} onClick={() => {
                                 confirm("Are you sure you want to delete this overlay?") && deleteOverlay(overlay.ID)
                             }}>Delete</button>
-                            <span className="absolute z-50 hidden mt-2 p-2 w-48 text-center bg-black/75 text-white rounded tooltip-text group-hover:block pointer-events-none">
+                            {!isGempir && <span className="absolute z-50 hidden mt-2 p-2 w-48 text-center bg-black/75 text-white rounded tooltip-text group-hover:block pointer-events-none">
                                 Only gempir can
-                            </span>
+                            </span>}
                         </div>
                     </div>
                     <div>{overlay.ID}</div>
