990cc63ebe6cf039231605de0701b138acfdf3d4
gempir <gempir.dev@gmail.com>
2025-12-06 16:59:28 +0100

    tsc

--- web/src/service/isNumeric.ts
+++ web/src/service/isNumeric.ts
@@ -1,6 +1,5 @@
 export function isNumeric(str: string): boolean {
   if (typeof str !== "string") return false; // we only process strings!
-  // @ts-expect-error
   return (
     !Number.isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
     !Number.isNaN(parseFloat(str))
